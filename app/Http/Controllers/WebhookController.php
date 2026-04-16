cca35629a5e37fbc99e1cfd9bdf77dfe4ec8db4c
kevin Rivera <kriverarojas44@gmail.com>
2026-04-16 15:28:53 -0500

    actualizacion3

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -8,9 +8,14 @@ class WebhookController extends Controller
 {
     public function github(Request $request)
     {
-       putenv('HOME=/home/tukipu');
-       putenv('PATH=/usr/local/bin:/usr/bin:/bin');
-       shell_exec('dploy deploy master');
-       return response('Webhook recibido', 200);
+        $payload = $request->getContent();
+        $signature = 'sha1=' . hash_hmac('sha1', $payload, env('GITHUB_WEBHOOK_SECRET'));
+        if ($request->header('X-Hub-Signature') !== $signature) {
+            return response('Firma no válida', 403);
+        }
+        putenv('HOME=/home/tukipu'); 
+        putenv('PATH=/usr/local/bin:/usr/bin:/bin');
+        shell_exec('dploy deploy master');
+        return response('Webhook recibido', 200);
     }
 }
