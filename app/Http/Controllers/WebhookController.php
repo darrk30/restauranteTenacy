7fa9a65009fe192199d4432776ef145e25f3caf7
kevin Rivera <kriverarojas44@gmail.com>
2026-04-16 22:21:23 -0500

    krivera

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -7,14 +7,33 @@ use Illuminate\Http\Request;
 
 class WebhookController extends Controller
 {
+    // public function github(Request $request)
+    // {
+    //     $payload = $request->getContent();
+    //     $signature = 'sha1=' . hash_hmac('sha1', $payload, 'deployresttukipu2026@');
+    //     if ($request->header('X-Hub-Signature') !== $signature) {
+    //         return response('Firma no válida', 403);
+    //     }
+    //     GithubWebhook::dispatch();//job
+    //     return response('Webhook recibido', 200);
+    // }
     public function github(Request $request)
     {
         $payload = $request->getContent();
-        $signature = 'sha1=' . hash_hmac('sha1', $payload, 'deployresttukipu2026@');
+        $secret = 'deployresttukipu2026@';
+        $signature = 'sha1=' . hash_hmac('sha1', $payload, $secret);
+
         if ($request->header('X-Hub-Signature') !== $signature) {
+            \Log::error('Firma de GitHub no coincide', [
+                'recibida' => $request->header('X-Hub-Signature'),
+                'calculada' => $signature
+            ]);
             return response('Firma no válida', 403);
         }
-        GithubWebhook::dispatch();//job
+
+        \Log::info('Webhook recibido correctamente, despachando Job...');
+        GithubWebhook::dispatch();
+        
         return response('Webhook recibido', 200);
     }
 }
